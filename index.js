1a335e569c49318f9be8d3f692010b83fdf66ccf
codewithshamim <shamimislamonline@gmail.com>
2022-05-04 20:11:24 +0600

    update quantity api created

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ async function run() {
             res.send(result);
 
         })
+
+        // ---update quantity---
+        app.put('/updateQuantity/:id', async(req, res) => {
+            const id = req.params.id;
+            const quantity = req.body.newQuantity;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: parseInt(quantity)
+                },
+            };
+            const result = await itemsCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+
+        })
     } finally {
         // await client.close();
     }
