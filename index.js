b16fd59ad1e3e2cf2101ff153345596c3c84eee4
codewithshamim <shamimislamonline@gmail.com>
2022-05-05 11:26:40 +0600

    delete api created

--- index.js
+++ index.js
@@ -54,6 +54,15 @@ async function run() {
             res.json(result)
 
         })
+
+        // ---delete item---
+        app.delete('/deleteItem/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        })
     } finally {
         // await client.close();
     }
