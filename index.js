cdfea50e8d398de9556521a1939de26bd76ae219
codewithshamim <shamimislamonline@gmail.com>
2023-03-09 09:25:43 +0600

    deploy2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 5000;
 const databaseConntect = require("./utils/database");
 const bookRouter = require("./routes/book.route");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
